3910c8be6f1abda274deee69b83b8fa4757e884b
kallcyte <kallcyte@gmail.com>
2025-12-31 20:05:54 +0700

    feat: Introduce an emoji picker script, update Hyprland and Hypremoji configurations, refine song detail display, and remove old log files.

--- config/hypr/scripts/songdetail.sh
+++ config/hypr/scripts/songdetail.sh
@@ -4,7 +4,7 @@ SONG=$(playerctl metadata --format '{{title}} - {{artist}}' 2>/dev/null)
 
 # If the song is empty (nothing playing), show nothing or a custom message
 if [ -z "$SONG" ]; then
-  echo "No Media Playing"
+  echo ""
 else
-  echo "$SONG"
+  echo "󰎇 $SONG"
 fi
